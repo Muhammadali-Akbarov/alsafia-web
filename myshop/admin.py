dfcc5463df97685372f748d76ef23a049f14cb16
Muhammadali-Akbarov <muhammadali17abc@gmail.com>
2022-12-13 15:42:16 +0500

    Modified SMSClient Token ⚙️

--- myshop/admin.py
+++ myshop/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
+
 from myshop.models.cart import Cart
+from myshop.models.sms import SMSClient
 from myshop.models.products import Products
 from myshop.models.categories import Categories
 from myshop.models.order_history import OrderHistory
@@ -15,3 +17,4 @@ admin.site.register(Categories)
 admin.site.register(Products, ProductsAdmin)
 admin.site.register(Cart)
 admin.site.register(OrderHistory)
+admin.site.register(SMSClient)
